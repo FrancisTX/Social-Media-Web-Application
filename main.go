7a2a18194a290dce77b572685a22bc7a557b3c14
Ming-Han Huang <mh6069@nyu.edu>
2021-11-28 06:56:31 -0500

    simple login

--- main.go
+++ main.go
@@ -5,34 +5,13 @@ import (
 	"main/web/auth"
 	"net/http"
 	"net/url"
-	"errors"
 )
 
 func LoginAuth(c *gin.Context) {
-	var (
-		username string
-		password string
-	)
-	if in, isExist := c.GetPostForm("username"); isExist && in != "" {
-		username = in
-	} else {
-		c.HTML(http.StatusBadRequest, "login.html", gin.H{
-			"error": errors.New("Must enter username"),
-		})
-		return
-	}
-	if in, isExist := c.GetPostForm("password"); isExist && in != "" {
-		password = in
-	} else {
-		c.HTML(http.StatusBadRequest, "login.html", gin.H{
-			"error": errors.New("Must enter password"),
-		})
-		return
-	}
+	var username, _ = c.GetPostForm("username")
+	var password, _ = c.GetPostForm("password")
+
 	if err := auth.Auth(username, password); err == nil {
-		//c.HTML(http.StatusOK, "login.html", gin.H{
-		//	"success": "Login successful",
-		//})
 		location := url.URL{Path: "/",}
     	c.Redirect(http.StatusFound, location.RequestURI())
 		return
