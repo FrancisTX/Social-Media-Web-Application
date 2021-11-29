38868fcb679ea793e31e0ad8bf17094723270dca
Ming-Han Huang <mh6069@nyu.edu>
2021-11-29 02:32:30 -0500

    home/profile page

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func LoginAuth(c *gin.Context) {
 	var password, _ = c.GetPostForm("password")
 
 	if err := auth.Auth(username, password); err == nil {
-		location := url.URL{Path: "/",}
+		location := url.URL{Path: "/home",}
     	c.Redirect(http.StatusFound, location.RequestURI())
 		return
 	} else {
@@ -23,6 +23,22 @@ func LoginAuth(c *gin.Context) {
 	}
 }
 
+func LogOut(c *gin.Context) {
+	location := url.URL{Path: "/login",}
+    c.Redirect(http.StatusFound, location.RequestURI())
+}
+
+func NavHome(c *gin.Context) {
+	location := url.URL{Path: "/home",}
+    c.Redirect(http.StatusFound, location.RequestURI())
+}
+
+func NavProfile(c *gin.Context) {
+	location := url.URL{Path: "/profile",}
+    c.Redirect(http.StatusFound, location.RequestURI())
+
+}
+
 func LoginPage(c *gin.Context) {
 	c.HTML(http.StatusOK, "login.html", nil)
 }
@@ -31,12 +47,20 @@ func MainPage(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", nil)
 }
 
+func ProfilePage(c *gin.Context) {
+	c.HTML(http.StatusOK, "profile.html", nil)
+}
+
 func main() {
 	server := gin.Default()
 	server.LoadHTMLGlob("src/html/*")
 	server.Static("/assets", "./src/assets")
-	server.GET("/", MainPage)
+	server.GET("/home", MainPage)
+	server.GET("/profile", ProfilePage)
 	server.GET("/login", LoginPage)
 	server.POST("/login", LoginAuth)
+	server.POST("/logout", LogOut)
+	server.POST("/navprofile", NavProfile)
+	server.POST("/navhome", NavHome)
 	server.Run(":8888")
 }
--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -6,3 +6,33 @@ switchers.forEach(item => {
 		this.parentElement.classList.add('is-active')
 	})
 })
+
+
+const imgDiv = document.querySelector('.profile-pic-div');
+const img = document.querySelector('#photo');
+const file = document.querySelector('#file');
+const uploadBtn = document.querySelector('#uploadBtn');
+
+imgDiv.addEventListener('mouseenter', function(){
+    uploadBtn.style.display = "block";
+});
+
+
+imgDiv.addEventListener('mouseleave', function(){
+    uploadBtn.style.display = "none";
+});
+
+file.addEventListener('change', function(){
+    const choosedFile = this.files[0];
+
+    if (choosedFile) {
+
+        const reader = new FileReader(); 
+
+        reader.addEventListener('load', function(){
+            img.setAttribute('src', reader.result);
+        });
+
+        reader.readAsDataURL(choosedFile);
+    }
+});
